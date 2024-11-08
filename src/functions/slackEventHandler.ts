99ce4913d137e7cd76352425c223aca3622cd103
Veetiknen <koistinenveet107@gmail.com>
2024-11-08 12:29:19 +0200

    Bot can now be hosted from AWS Lambda and uses serverless (#3)
    * Bot can now be hosted from AWS Lambda and uses serverless
    
    * small change to README.md
    
    * naming and readme fixes
    
    * Slackbot in a class
    
    * small fixes
    
    * package-lock regen
    
    * prettier fixes
    
    ---------
    
    Co-authored-by: Veeti Koistinen <veeti.koistinen@sadeinnovations.com>

---
+++ src/functions/slackEventHandler.ts
@@ -0,0 +1,23 @@
+import { APIGatewayProxyEvent, Context, Callback } from "aws-lambda";
+import { AwsResponse } from "@slack/bolt/dist/receivers/AwsLambdaReceiver";
+import { SlackBot } from "./utils/slackBot";
+
+export async function handleSlackEvent(
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback: Callback
+): Promise<AwsResponse> {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+
+  const slackBot = new SlackBot();
+
+  try {
+    return await slackBot.processEvent(event, context, callback);
+  } catch (error) {
+    console.error("Error processing the request:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
+}
